8f24618de9a9febbbb3231a1a9dd110934dcb3f6
Marin Cabac <marin.cabac@gmail.com>
2018-03-09 01:46:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,9 @@ class Transfer
 
       "Transaction rejected. Please check your account balance."
   else
+     sender.balance -= @amount
       receiver.balance += @amount
-   sender.balance -= @amount
+
    @status="complete"
 
 
