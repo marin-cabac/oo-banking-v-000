b0444f3d1804c086ce2c3fae43644d28707bf006
Marin Cabac <marin.cabac@gmail.com>
2018-03-09 01:42:56 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
+attr_accessor :balance, :status
+attr_reader :name
+
+  def initialize(name)
+
+    @name = name
+    @balance=1000
+    @status="open"
+  end
+  def deposit(int)
+    @balance+=int
+  end
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  def  valid?
+    @status=="open" && @balance > 0
+  end
+  def close_account
+    self.status="closed"
+     
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
+  attr_accessor :status, :sender, :receiver, :amount
   # your code here
+  def initialize(sender, receiver, amnt)
+    @sender=sender
+    @receiver=receiver
+    @amount=amnt
+    @status="pending"
+  end
+  def  valid?
+    sender.valid? && receiver.valid?
+  end
+  def  execute_transaction
+    if sender.valid? && status == "pending"
+
+      receiver.balance += @amount
+   sender.balance -= @amount
+   @status="complete"
+ else
+   @status="rejected"
+
+   "Transaction rejected. Please check your account balance."
+ end
+  end
+
+  def reverse_transfer
+if @status == "complete"
+          receiver.balance -= @amount
+       sender.balance += @amount
+       @status="reversed"
+     end 
+
+end
 end
