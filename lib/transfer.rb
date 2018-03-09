a4970d4e1091bef173eb3549ccb167e1f4be868c
Marin Cabac <marin.cabac@gmail.com>
2018-03-09 01:56:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,7 +12,7 @@ class Transfer
   end
 
   def execute_transaction
-     if self.valid? && self.status != "complete" && @sender.balance > @amount
+     if self.valid? && @status != "complete" && @sender.balance > @amount
         @sender.balance -= @amount
         @receiver.balance += @amount
        self.status = "complete"
